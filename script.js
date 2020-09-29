c15b9d890d9c3bf533bd1fe17b260fcfd1f48b99
Dr Phil <philip.prewitt@gmail.com>
2020-09-29 10:41:10 -0600

    rewinding script

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-var languageMode;
-function selectMode() {
-    var languageMode = document.getElementById("dropdownMenuLink").value;
-}
 var myCodeMirror = CodeMirror(document.getElementById("demo"), {
     value: "function myScript(){return 100;}\n",
-    mode: languageMode
+    mode:  "javascript"
   });
-
